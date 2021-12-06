e6f852bff6ecd18eb433d57289337727f1bf687a
Aurino Geraldo <aurinoaj@gmail.com>
2021-12-05 22:38:25 -0300

    Using props on BtnHeader Style

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
-import { Header } from "./styles";
+import { Header, BtnHeader } from "./styles";
 
 const Cabecalho = () => {
   return (
     <Header>
       <img height="50" width="50" src={bank_logo} alt="Logo Smart Bank" />
       <nav>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        </BtnHeader>
+        <BtnHeader href="https://google.com">Sair</BtnHeader>
       </nav>
     </Header>
   );
--- src/Components/Cabecalho/styles.js
+++ src/Components/Cabecalho/styles.js
@@ -10,3 +10,15 @@ export const Header = styled.header`
   height: 10vh;
   align-items: center;
 `;
+
+export const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props) => props.primary ? "white" : corPrimaria};
+  color: ${(props) => props.primary ? corPrimaria : "white"};
+`
