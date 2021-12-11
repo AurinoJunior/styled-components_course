fe7787fed3f1fb8987b0448ef47c53cd6bcbd91a
Aurino Geraldo <aurinoaj@gmail.com>
2021-12-11 17:56:08 -0300

    Separete color theme

--- src/Components/UI/Icone.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default ({ src, alt }) => {
-  return <img width="25" height="25" src={src} alt={alt} />;
-};
--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -1,5 +1,15 @@
-import Icone from './Icone'
+import React from "react";
+import styled from "styled-components";
 
-export {
-  Icone,
-}
+export const Icone = ({ src, alt }) => {
+  return <img width="25" height="25" src={src} alt={alt} />;
+};
+
+export const ButtonTema = styled.button`
+  position: absolute;
+  top: 4vh;
+  right: 20px;
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+`;
